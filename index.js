04149e72b0c486c0caeaf7419e679c6f8c6a5151
ittp <61617660+ittp@users.noreply.github.com>
2021-01-30 14:51:50 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+const logger = require("koa-logger");
+const bodyParser = require("koa-bodyparser");
+
+const PORT = process.env.PORT || 8000;
+
+const app = new Koa();
+const router = new Router();
+
+router
+  .get("/", async (ctx) => {
+    const name = ctx.query.name || "stranger";
+    ctx.body = {
+      message: `Hello, ${name}!`
+    };
+  })
+  .post("/", async (ctx, next) => {});
+
+app
+  .use(logger())
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(PORT, "0.0.0.0", () =>
+    console.log(`listening on http://localhost:${PORT}...`)
+  );
